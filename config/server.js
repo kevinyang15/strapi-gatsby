e338c6f7108aa051c6347145216bc4b69764a60b
kyy15 <102763650+kyy15@users.noreply.github.com>
2022-06-13 19:29:08 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3f4ae2d378c74195b595ca39339bf5e8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7d819e84-a8bc-47c3-9481-9cfdbea11a23'
+};
